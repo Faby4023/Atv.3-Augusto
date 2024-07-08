1113185cbc00982fc598e612d8277089e543c4c2
Faby4023 <143666861+Faby4023@users.noreply.github.com>
2024-07-08 11:11:18 -0300

    Add files via upload

---
+++ Ati.Faby2/index.js
@@ -0,0 +1,27 @@
+//Questão 1
+// let frase = prompt("Digite a primeira frase: ");
+// console.log(frase);
+// console.log(typeof(frase));
+//Questão 2
+// let string = "123";
+// console.log(typeof Number(string));
+// console.log(typeof String(string));
+// let number = 321
+// console.log(typeof(String(string)));
+//Questão 3
+// let numero1 = prompt("Digite o primeiro numero: ");
+// let numero2 = prompt("Digite o segundo numero: ");
+// let resultado = Number(numero1) + Number(numero2);
+// console.log(resultado)
+//Questão 4
+// string1 = "Estudar"
+// string2 = "é bom!"
+// console.log(string1 + string2)
+//Questão 5
+// let nome = prompt("Digite seu nome:");
+// if (nome !== null && nome !== ""){
+//     console.log(`Olá ${nome}, Bem vindo!`);
+// } else{
+//     console.log("Você não inseriu um nome válido");
+// }
+//Questão 6
